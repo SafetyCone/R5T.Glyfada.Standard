82dd99c92e25148fd83a8a80e59aa8b343880072
MinexAutomation <minex.automation@gmail.com>
2020-07-21 15:35:52 -0700

    Added AddXAction() method for GitOperator.

--- source/R5T.Glyfada.Standard/Code/Extensions/IServiceCollectionExtensions.cs
+++ source/R5T.Glyfada.Standard/Code/Extensions/IServiceCollectionExtensions.cs
@@ -1,18 +1,49 @@
 ﻿using System;
 
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 using R5T.Caledonia;
 using R5T.Caledonia.Default;
-using R5T.Glyfada.Default;
+using R5T.Dacia;
 using R5T.Nikaia;
 using R5T.Nikaia.Configuration;
+
+using R5T.Glyfada.Default;
 
 
 namespace R5T.Glyfada.Standard
 {
     public static class IServiceCollectionExtensions
     {
+        public static
+            (
+            IServiceAction<IGitOperator> main,
+            IServiceAction<ICommandLineInvocationOperator> commandLineInvocationOperatorAction,
+            IServiceAction<IGitExecutableFilePathProvider> gitExecutableFilePathProviderAction,
+            IServiceAction<IOptions<GitConfiguration>> gitConfigurationOptionsAction
+            )
+        AddGitOperatorAction(this IServiceCollection services)
+        {
+            // 0.
+            var commandLineInvocationOperatorAction = ServiceAction<ICommandLineInvocationOperator>.New(() => services.AddSingleton<ICommandLineInvocationOperator, DefaultCommandLineInvocationOperator>());
+            var gitConfigurationAction = services.AddGitConfigurationAction();
+
+            // 1.
+            var gitExecutableFilePathProviderAction = services.AddGitExecutableFilePathProviderAction(
+                gitConfigurationAction);
+
+            var gitOperatorAction = services.AddGitOperatorAction(
+                commandLineInvocationOperatorAction,
+                gitExecutableFilePathProviderAction);
+
+            return (
+                gitOperatorAction,
+                commandLineInvocationOperatorAction,
+                gitExecutableFilePathProviderAction,
+                gitConfigurationAction);
+        }
+
         public static IServiceCollection AddGitOperator(this IServiceCollection services)
         {
             services
